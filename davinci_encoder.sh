bfdc1f413236d68bd66a6e06d2fc12fbd03abb55
StavWasPlayZ <stavsolomon101@gmail.com>
2024-10-10 09:56:53 +0300

    Add out path for davinci encoder

--- davinci_encoder.sh
+++ davinci_encoder.sh
@@ -4,21 +4,25 @@
 # Source: https://www.youtube.com/watch?v=WLcW4UWPC5Y&lc=UgzCqHeTZjg1ms_5ATR4AaABAg
 
 usage_err() {
-    echo "Usage: $0 <e|encode|d|decode> <input_file>"
+    echo "Usage: $0 <e|encode|d|decode> <input_file> [out_path]"
     exit 1
 }
 
-if [ "$#" -ne 2 ]; then
+out_path="./"
+
+if [ "$#" -e 3 ]; then
+    out_path=$3
+elif [ "$#" -ne 2 ]; then
     usage_err
 fi
 
 
 encode_davinci() {
     # ffmpeg -i "$1" -vcodec mjpeg -q:v 2 -acodec pcm_s16be -q:a 0 -f mov "$1.encoded.mov"
-    ffmpeg -i "$1" -c:v mpeg4 -q:v 2 -c:a flac "${1}.flac.mp4"
+    ffmpeg -i "$1" -c:v mpeg4 -q:v 2 -c:a flac "${out_path/}/${1}.flac.mp4"
 }
 decode_davinci() {
-    ffmpeg -i "$1" -c:v libx264 -preset ultrafast -crf 0 "${1}.out.mp4"
+    ffmpeg -i "$1" -c:v libx264 -preset ultrafast -crf 0 "${out_path/}/${1}.out.mp4"
 }
 
 case "$1" in
